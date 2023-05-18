877bc51615d615acaa7412b3d986b0de4c322ab5
goldnite <goldendevens@gmail.com>
2023-05-18 17:17:55 -0400

    update email notify

--- scripts/notify.js
+++ scripts/notify.js
@@ -1,9 +1,25 @@
 const hre = require('hardhat');
+const Nylas = require('nylas');
+const { default: Draft } = require('nylas/lib/models/draft');
+
+Nylas.config({
+  clientId: process.env.NYLAS_CLIENT_ID,
+  clientSecret: process.env.NYLAS_CLIENT_SECRET,
+});
+
+const nylas = Nylas.with(process.env.NYLAS_ACCESS_TOKEN);
+
+const sendMail = async function (attacker, timestamp) {
+  const draft = new Draft(nylas, {
+    subject: 'Attack Detected',
+    body: `There was an reentrancy attack at ${timestamp} from address ${attacker} to your Reentrance contract. Pause the contract shortly.`,
+    to: [{ name: 'Admin', email: 'forzecakko@gufum.com' }],
+  });
+  const message = await draft.send();
+  console.log(`Mail id ${message.id} was sent.`);
+};
 
 async function main() {
-  const [owner] = await ethers.getSigners();
-  const provider = ethers.provider;
-
   const reentrance = await ethers.getContractAt(
     'Reentrance',
     process.env.REENTRANCE_ADDRESS
@@ -11,11 +27,12 @@ async function main() {
 
   const reentrancyAttack = reentrance.filters.ReentrancyAttack();
   reentrance.on(reentrancyAttack, async function (attacker, event) {
-    console.log(
-      `ReentrancyAttack from ${attacker} at ${new Date(
-        (await event.getBlock()).timestamp * 1000
-      ).toISOString()}`
-    );
+    const timestamp = new Date(
+      (await event.getBlock()).timestamp * 1000
+    ).toISOString();
+    console.log(`ReentrancyAttack from ${attacker} at ${timestamp}`);
+    console.log('Sending an email ...');
+    // await sendMail(attacker, timestamp);
   });
 }
 
